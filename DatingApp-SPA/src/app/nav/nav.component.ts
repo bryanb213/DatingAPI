cfadb8822469977dd2f50d4e5a3f5d0c99da81cf
bryanb213 <blancasbryan@gmail.com>
2019-09-19 15:45:23 -0500

    added routing

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,16 +13,18 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
 
   login() {
-    this.authService.login(this.model).subscribe(next => {
+    this.authService.login(this.model).subscribe(next => {// next
       this.alertify.success('logged in Successfully');
-    }, error => {
+    }, error => {                                         // error
       this.alertify.error(error.statusText);
+    }, () => {                                            // finally
+      this.router.navigate(['/members']);
     });
   }
 
@@ -32,6 +35,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 
 }
---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,27 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+
+
+
+// add protected routhes as children routes with 1 authGuard or add canActivate: [AuthGuard] to every protected component
+export const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent},
+            { path: 'messages', component: MessagesComponent},
+            { path: 'lists', component: ListsComponent},
+        ]
+    },
+    // use empty '' to redirect to home component
+    {path: '**', redirectTo: '', pathMatch: 'full'}
+];
+// use full to grab all url to redirect
